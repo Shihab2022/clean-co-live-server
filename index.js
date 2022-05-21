ea8856ea6bdaebebe5e90035ab9c7901a279681c
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-21 12:51:50 +0600

    add post and delete method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express()
 const port =process.env.PORT || 5000
 require("dotenv").config();
@@ -15,11 +15,46 @@ async function run() {
       await client.connect();
      const serviceCollection= client.db("cleanCo").collection("service");
 
-     app.get('/services',async(req,res)=>{
+/**
+ * get /service =>all data
+ * post / add-service => create new data
+ * put / update-service =>modify a data on collection
+ * delete / delete-service => delete a data from collection 
+ */
 
+     app.get('/get-services',async(req,res)=>{
         const service=await serviceCollection.find({}).toArray()
         res.send(service)
      })
+
+     app.post ('/add-service',async(req,res)=>{
+         const data=req.body;
+         const result =await serviceCollection.insertOne(data);
+         res.send(result)
+     })
+     app.put('/update-service/:id',async(req,res)=>{
+
+        const id=req.params.id;
+        const data=req.body;
+        const filter={_id:ObjectId(id)}
+        const updateDoc={$set : data}
+        const option={upsert : true}
+        const result = await serviceCollection.updateOne(filter,updateDoc,option)
+        res.send(result)
+     })
+     app.delete('/delete-service/:id',async(req,res)=>{
+
+        const id=req.params.id;
+      
+        const filter={_id:ObjectId(id)}
+        
+        const result = await serviceCollection.deleteOne(filter)
+        res.send(result)
+     })
+
+
+
+
  
     }
     
@@ -39,6 +74,16 @@ async function run() {
 // });
 
 
+app.get('/user/data',async(req,res)=>{
+    const data=req.body
+    res.send(data)
+})
+
+app.get('/user/:id',async(req,res)=>{
+    const id =req.params.id
+    console.log(id)
+    res.send(id)
+})
 app.get('/',async(req,res)=>{
     res.send('clinco ir running')
 })
